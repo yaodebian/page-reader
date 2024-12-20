d2245cfcac04c03be8384423bce0d56130e8d903
yaodebian <2801540120@qq.com>
2024-12-20 23:51:36 +0800

    fix: cannot upload file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,20 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
-
-#[tauri::command]
-async fn read_file(path: String) -> Result<Vec<u8>, String> {
-    match std::fs::read(path) {
-        Ok(contents) => Ok(contents),
-        Err(e) => Err(e.to_string()),
-    }
-}
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![read_file])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    page_reader_lib::run()
 }
